8a821b5c4619730f9236fd30dd893d3652b4648b
Sonister <yeetbareck@gmail.com>
2021-10-04 22:26:06 +0300

    Add tape equilibrium function

--- src/algs/manyahl/net/Main.java
+++ src/algs/manyahl/net/Main.java
@@ -1,6 +1,22 @@
 package algs.manyahl.net;
 
 public class Main {
+    public static int tapeEquilibrium(int[] A){
+        int N = A.length;
+        int result=Integer.MAX_VALUE;
+        int leftSum = A[0];
+        int rightSum = 0;
+        for(int i=1;i<N;i++)    rightSum+=A[i];
+        int diff=Math.abs(leftSum-rightSum);
+        if(diff<result) result=diff;
+        for(int i = 1;i < N -1;i++){
+            leftSum+=A[i];
+            rightSum-=A[i];
+            diff=Math.abs(leftSum-rightSum);
+            if(diff<result) result=diff;
+        }
+        return result;
+    }
     public static int specialString(String str){
         int n = str.length();
         for (int i = 0; i < str.length(); i++) {
